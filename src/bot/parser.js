e052325939f8dfe2be836b5af4e9f48571eb5256
Júlia Santos <julia.lima.713@ufrn.edu.br>
2026-06-09 16:19:14 -0300

    Add: Categorias

---
+++ src/bot/parser.js
@@ -0,0 +1,14 @@
+const CATEGORIAS = {
+  alimentacao: ['mercado', 'supermercado', 'feira', 'açougue', 'padaria', 'restaurante', 'lanche', 'pizza', 'comida', 'almoço', 'jantar', 'café', 'ifood', 'rappi', 'uber eats'],
+  transporte: ['uber', 'taxi', 'ônibus', 'combustível', 'gasolina', 'etanol', 'passagem', 'metro', 'estacionamento', '99', 'cabify'],
+  saude: ['farmácia', 'remédio', 'médico', 'consulta', 'exame', 'hospital', 'dentista', 'plano de saúde', 'academia'],
+  moradia: ['aluguel', 'condomínio', 'água', 'luz', 'energia', 'internet', 'telefone', 'gás', 'iptu', 'reforma'],
+  lazer: ['cinema', 'show', 'teatro', 'viagem', 'hotel', 'passeio', 'streaming', 'netflix', 'spotify', 'jogo'],
+  educacao: ['curso', 'faculdade', 'escola', 'livro', 'material', 'mensalidade'],
+  roupas: ['roupa', 'sapato', 'tênis', 'calça', 'camisa', 'vestido', 'loja'],
+  salario: ['salário', 'pagamento', 'holerite', 'contracheque'],
+  freelance: ['freelance', 'freela', 'trabalho extra', 'projeto', 'consultoria', 'serviço'],
+  investimento: ['dividendo', 'rendimento', 'juros', 'cdb', 'tesouro', 'ação', 'fundo'],
+  outros: [''],
+};
+
